6bb07da100183a558f4ed5d11deb4bfa0f8058b1
Mohammad Davodi <mohammaddavodi96@gmail.com>
2023-11-14 15:00:18 +0330

    services

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import TableCoin from "../modules/TableCoin";
+import { getCryptoList } from "../../services/cryptoAPI";
 
 const HomePage = () => {
 
     const [coins, setCoins] = useState([]);
 
     useEffect(() =>{
-        fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en")
+        fetch(getCryptoList())
         .then(res => res.json())
         .then(data => setCoins(data))
     }, [])
