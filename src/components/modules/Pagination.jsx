cb97a3f431122402316f0b76cb9faf67a33b1572
Mohammad Davodi <mohammaddavodi96@gmail.com>
2023-11-21 13:48:24 +0330

    paginate

--- src/components/modules/Pagination.jsx
+++ src/components/modules/Pagination.jsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
 
 const Pagination = () => {
-  
-    const [page, setPage] = useState(1);
+  const [page, setPage] = useState(1);
 
-    const preHandler = () => {
-        if(page <= 1) return;
-        setPage((page) => page - 1)
-    }
+  const preHandler = () => {
+    if (page <= 1) return;
+    setPage((page) => page - 1);
+  };
 
-    const nextHandler = () => {
-        if(page >= 20) return;
-        setPage((page) => page + 1)
-    }
+  const nextHandler = () => {
+    if (page >= 10) return;
+    setPage((page) => page + 1);
+  };
 
   return (
     <div>
       <button onClick={preHandler}>Previous</button>
-      <p>{page}</p>
+      <p style={{ color: page === 1 ? "green" : "inherit" }}>1</p>
+      <p>2</p>
+      {page > 2 && page < 9 && (
+        <>
+          <span>...</span>
+          <p>{page}</p>
+        </>
+      )}
+      <span>...</span>
+      <p>9</p>
+      <p>10</p>
       <button onClick={nextHandler}>Next</button>
     </div>
   );
