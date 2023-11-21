5fc335f023d69e5ea44c41aa5288dfe167d55e60
Mohammad Davodi <mohammaddavodi96@gmail.com>
2023-11-21 13:38:09 +0330

    pageHandlers

--- src/components/modules/Pagination.jsx
+++ src/components/modules/Pagination.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 const Pagination = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  
+    const [page, setPage] = useState(1);
+
+    const preHandler = () => {
+        if(page <= 1) return;
+        setPage((page) => page - 1)
+    }
+
+    const nextHandler = () => {
+        if(page >= 20) return;
+        setPage((page) => page + 1)
+    }
+
+  return (
+    <div>
+      <button onClick={preHandler}>Previous</button>
+      <p>{page}</p>
+      <button onClick={nextHandler}>Next</button>
+    </div>
+  );
 };
 
 export default Pagination;
