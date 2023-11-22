481337f9c64777ab700b680259d6c8fd153dd740
Mohammad Davodi <mohammaddavodi96@gmail.com>
2023-11-22 21:54:43 +0330

    currency

---
+++ src/components/modules/Search.jsx
@@ -0,0 +1,15 @@
+
+function Search() {
+    return (
+        <div>
+            <input type="text" />            
+            <select>
+                <option value="">USD</option>
+                <option value="">EUR</option>
+                <option value="">JPY</option>
+            </select>
+        </div>
+    );
+}
+
+export default Search;
